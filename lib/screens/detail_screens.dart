7a43c0cba182065dcce54d1e15d3da87fe117715
Justyn Cannavaro <justyncannavaro_2428250027@mhs.mdp.ac.id>
2025-10-16 13:17:45 +0700

    p

--- lib/screens/detail_screens.dart
+++ lib/screens/detail_screens.dart
@@ -40,7 +40,7 @@ class DetailScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ],
